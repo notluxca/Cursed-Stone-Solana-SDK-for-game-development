87b2a608244036ce3d539bca20ac7dc1eba6ac40
Lucas Fernandes <84829810+notluxca@users.noreply.github.com>
2023-09-13 17:00:07 -0300

    Create runExe.cs

---
+++ runExe.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class runExe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Process process = new Process();
+		process.StartInfo.FileName = "connect.exe";
+		process.StartInfo.Arguments = "";
+        process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+		process.Start();
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
